eb6bffa3fc4f280e0fd4a95f39ce339b2aa826df
Talha Faisal <talha.faisal2000@gmail.com>
2024-02-02 14:40:48 +0500

    Updated service content

--- components/Services.js
+++ components/Services.js
@@ -1,92 +1,5 @@
 import Link from "next/link";
 
-const Services = ({ extraClass }) => {
-  const services = [
-    {
-      id: 1,
-      title: "Brand Identity Design",
-      text: "Dignissimos ducimus blanditiis praesen",
-    },
-    {
-      id: 2,
-      title: "Website Design",
-      text: "Dignissimos ducimus blanditiis praesen",
-    },
-    {
-      id: 3,
-      title: "Mobile Application Design",
-      text: "Dignissimos ducimus blanditiis praesen",
-    },
-    {
-      id: 4,
-      title: "Motion Graphics Design",
-      text: "Dignissimos ducimus blanditiis praesen",
-    },
-    {
-      id: 5,
-      title: "Website Development",
-      text: "Dignissimos ducimus blanditiis praesen",
-    },
-    {
-      id: 6,
-      title: "SEO & Digital Marketing",
-      text: "Dignissimos ducimus blanditiis praesen",
-    },
-  ];
-  return (
-    <section
-      id="services"
-      className={`services-area pt-130 rpt-100 pb-100 rpb-70 rel z-1 ${extraClass}`}
-    >
-      <div className="container">
-        <div className="row justify-content-center">
-          <div className="col-xl-6 col-lg-8">
-            <div className="section-title text-center mb-60 wow fadeInUp delay-0-2s">
-              <span className="sub-title mb-15">Popular Services</span>
-              <h2>
-                My <span>Special Service</span> For your Business Development
-              </h2>
-            </div>
-          </div>
-        </div>
-        <div className="row">
-          {services.map((service) => (
-            <div className="col-lg-6" key={service.id}>
-              <div className="service-item wow fadeInUp delay-0-2s">
-                <div className="number">
-                  {service.id >= 9 ? service.id : `0${service.id}`}.
-                </div>
-                <div className="content">
-                  <h4>{service.title}</h4>
-                  <p>{service.text}</p>
-                </div>
-                <Link legacyBehavior href="/#">
-                  <a className="details-btn">
-                    <i className="fas fa-arrow-right" />
-                  </a>
-                </Link>
-              </div>
-            </div>
-          ))}
-        </div>
-      </div>
-      <div className="bg-lines">
-        <span />
-        <span />
-        <span />
-        <span />
-        <span />
-        <span />
-        <span />
-        <span />
-        <span />
-        <span />
-      </div>
-    </section>
-  );
-};
-export default Services;
-
 export const Services2 = () => {
   return (
     <section
@@ -98,9 +11,16 @@ export const Services2 = () => {
           <div className="col-xl-12">
             <div className="section-title text-center mb-50 wow fadeInUp delay-0-2s">
               <span className="sub-title mb-15">
-                <i className="flaticon-asterisk-1" /> my services area
+                <i className="flaticon-asterisk-1"></i> Expertise Unveiled
               </span>
               <h2>MY SERVICE AREA</h2>
+              <p class="mt-20">
+                Dive into a suite of professional services where creativity
+                meets technology. Offering comprehensive UI/UX design,
+                cutting-edge front-end development, and robust back-end
+                solutions to bring your visions to life with precision and
+                flair.
+              </p>
             </div>
           </div>
         </div>
@@ -110,19 +30,19 @@ export const Services2 = () => {
               <img src="assets/images/icons/service1.png" alt="Icon" />
             </div>
             <h5 className="title">
-              <a href="#">WEBSITE DEvelopment</a>
+              <a href="#contact">WEBSITE DEvelopment</a>
             </h5>
             <div className="text">
-              Welcome to our portfolio website! We are a prof printing company
-              that offers a wide range of Lo rem ipsum dolor sit amet,
-              consectetur
+              Specializing in seamless website development with a keen focus on
+              responsive design, user-centric interfaces, and cross-browser
+              compatibility to amplify your digital footprint.
             </div>
             <ul className="list">
-              <li>Ui/Ux Design</li>
-              <li>Research</li>
-              <li>Web &amp; Mobile app</li>
+              <li>Web Apps</li>
+              <li>CMS</li>
+              <li>Ecommerce</li>
             </ul>
-            <a href="#" className="details-btn">
+            <a href="#contact" className="details-btn">
               <i className="fal fa-long-arrow-right" />
             </a>
           </div>
@@ -131,19 +51,19 @@ export const Services2 = () => {
               <img src="assets/images/icons/service2.png" alt="Icon" />
             </div>
             <h5 className="title">
-              <a href="#">Digital Marketing</a>
+              <a href="#contact">UI/UX Design</a>
             </h5>
             <div className="text">
-              Welcome to our portfolio website! We are a prof printing company
-              that offers a wide range of Lo rem ipsum dolor sit amet,
-              consectetur
+              Delivering meticulously crafted UI/UX designs that blend
+              innovation with functionality, ensuring a captivating and
+              intuitive user experience across all digital platforms.
             </div>
             <ul className="list">
-              <li>Ui/Ux Design</li>
-              <li>Research</li>
-              <li>Web &amp; Mobile app</li>
+              <li>Wireframing</li>
+              <li>Prototyping</li>
+              <li>Interface Design</li>
             </ul>
-            <a href="#" className="details-btn">
+            <a href="#contact" className="details-btn">
               <i className="fal fa-long-arrow-right" />
             </a>
           </div>
@@ -152,19 +72,19 @@ export const Services2 = () => {
               <img src="assets/images/icons/service3.png" alt="Icon" />
             </div>
             <h5 className="title">
-              <a href="#">Front End DEvelopment</a>
+              <a href="#contact">Front End DEvelopment</a>
             </h5>
             <div className="text">
-              Welcome to our portfolio website! We are a prof printing company
-              that offers a wide range of Lo rem ipsum dolor sit amet,
-              consectetur
+              Building cutting-edge front-end interfaces utilizing the latest in
+              HTML5, CSS3, and JavaScript frameworks to create fast, responsive,
+              and dynamic user experiences.
             </div>
             <ul className="list">
-              <li>Ui/Ux Design</li>
-              <li>Research</li>
-              <li>Web &amp; Mobile app</li>
+              <li>React JS</li>
+              <li>Next JS</li>
+              <li>Angular</li>
             </ul>
-            <a href="#" className="details-btn">
+            <a href="#contact" className="details-btn">
               <i className="fal fa-long-arrow-right" />
             </a>
           </div>
@@ -173,25 +93,25 @@ export const Services2 = () => {
               <img src="assets/images/icons/service4.png" alt="Icon" />
             </div>
             <h5 className="title">
-              <a href="#">Joomla redesigning</a>
+              <a href="#contact">Back End Development</a>
             </h5>
             <div className="text">
-              Welcome to our portfolio website! We are a prof printing company
-              that offers a wide range of Lo rem ipsum dolor sit amet,
-              consectetur
+              Engineering robust back-end systems with advanced server-side
+              technologies, database management, and API integration to power
+              interactive and scalable web applications.
             </div>
             <ul className="list">
-              <li>Ui/Ux Design</li>
-              <li>Research</li>
-              <li>Web &amp; Mobile app</li>
+              <li>Python</li>
+              <li>Node JS</li>
+              <li>PHP</li>
             </ul>
-            <a href="#" className="details-btn">
+            <a href="#contact" className="details-btn">
               <i className="fal fa-long-arrow-right" />
             </a>
           </div>
         </div>
         <div className="services-more-btn text-center pt-55 wow fadeInUp delay-0-2s">
-          <Link legacyBehavior href="services" className="theme-btn">
+          <Link legacyBehavior href="#contact" className="theme-btn">
             Get more services
           </Link>
         </div>
