81970e1e441886889bf71ffcf48f028467fc2e16
Talha Faisal <talha.faisal2000@gmail.com>
2024-02-05 11:57:10 +0500

    Chnage Logo Font

--- layout/Header.js
+++ layout/Header.js
@@ -8,103 +8,15 @@ import { OnePageMenu } from "./Menu";
 const Header = ({ header, onePageMenu }) => {
   switch (header) {
     case 1:
-      return <Header1 onePageMenu={onePageMenu} />;
+      return <Header2 onePageMenu={onePageMenu} />;
     case 2:
       return <Header2 onePageMenu={onePageMenu} />;
 
     default:
-      return <Header1 onePageMenu={onePageMenu} />;
+      return <Header2 onePageMenu={onePageMenu} />;
   }
 };
 export default Header;
-
-const Header1 = ({ onePageMenu }) => {
-  useEffect(() => {
-    utilits.stickyNav();
-  }, []);
-
-  const toggleSidebar = () => {
-    document.querySelector("body").classList.add("side-content-visible");
-  };
-
-  const [toggle, setToggle] = useState(false);
-
-  return (
-    <header className="main-header menu-absolute">
-      {/*Header-Upper*/}
-      <div className="header-upper">
-        <div className="container container-1620 clearfix">
-          <div className="header-inner rel d-flex align-items-center">
-            <div className="logo-outer">
-              <div className="logo">
-                <Link legacyBehavior href="/">
-                  <a>
-                    <img
-                      src="assets/images/logos/logo.png"
-                      alt="Logo"
-                      title="Logo"
-                    />
-                  </a>
-                </Link>
-              </div>
-            </div>
-            <div className="nav-outer clearfix mx-auto">
-              {/* Main Menu */}
-              <nav className="main-menu navbar-expand-lg">
-                <div className="navbar-header">
-                  <div className="mobile-logo my-15">
-                    <Link legacyBehavior href="/">
-                      <a>
-                        <img
-                          src="assets/images/logos/logo.png"
-                          alt="Logo"
-                          title="Logo"
-                        />
-                      </a>
-                    </Link>
-                  </div>
-                  {/* Toggle Button */}
-                  <button
-                    type="button"
-                    className="navbar-toggle me-4"
-                    data-bs-toggle="collapse"
-                    data-bs-target=".navbar-collapse"
-                    onClick={() => setToggle(!toggle)}
-                  >
-                    <span className="icon-bar" />
-                    <span className="icon-bar" />
-                    <span className="icon-bar" />
-                  </button>
-                </div>
-                <div
-                  className={`navbar-collapse collapse clearfix ${
-                    toggle ? "show" : ""
-                  }`}
-                >
-                 <OnePageMenu />
-                </div>
-              </nav>
-              {/* Main Menu End*/}
-            </div>
-            {/* Menu Button */}
-            <div className="menu-btns d-none d-lg-block">
-              {/* menu sidbar */}
-              <div className="menu-sidebar">
-                <button onClick={() => toggleSidebar()}>
-                  <img
-                    src="assets/images/shape/sidebar-tottler.svg"
-                    alt="Toggler"
-                  />
-                </button>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      {/*End Header Upper*/}
-    </header>
-  );
-};
 
 const Header2 = ({ onePageMenu }) => {
   useEffect(() => {
@@ -137,15 +49,11 @@ const Header2 = ({ onePageMenu }) => {
               <nav className="main-menu navbar-expand-lg">
                 <div className="navbar-header">
                   <div className="mobile-logo my-15">
-                    <Link legacyBehavior href="/">
-                      <a>
-                        <img
-                          src="assets/images/logos/logo.png"
-                          alt="Logo"
-                          title="Logo"
-                        />
-                      </a>
-                    </Link>
+                  <Link legacyBehavior href="/">
+                  <a style={{display: "flex", gap: "15px", margin: "0px", fontSize:'20px'}} className="h3">
+                    <i style={{color:'#C9F31D'}} className="flaticon-asterisk-1"></i> Talha Faisal
+                  </a>
+                </Link>
                   </div>
                   {/* Toggle Button */}
                   <button
